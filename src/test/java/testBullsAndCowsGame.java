8eafd0e523ecd54aa097f81cbaf3447c173ea6dd
Alexey Gr <alex_gr2@mail.ru>
2024-03-16 16:38:27 +0700

    Create test class "Bulls and Cows"

---
+++ src/test/java/testBullsAndCowsGame.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class testBullsAndCowsGame {
+
+    @Test
+    void calcClassCreationTest() {
+        BullsAndCowsGame game = new BullsAndCowsGame();
+        Assertions.assertNotNull(game);
+    }
+}
